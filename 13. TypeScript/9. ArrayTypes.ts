16983a98a6d86eda4f1cbfdc70c7f4cd36c3e699
Aadidev Soni <aadidevbahrain@gmail.com>
2025-06-10 19:10:54 +0300

    Typescript Arrays

---
+++ 13. TypeScript/9. ArrayTypes.ts
@@ -0,0 +1,15 @@
+const nums: number[] = [1,2,3,4];
+console.log(nums);
+
+const str: string[] = ["one", "two", "three"];
+console.log(str);
+
+const items: string[] = [];
+items.push("mouse");
+console.log(items);
+
+const numList: number[] = [];
+numList.push(20);
+numList.push(2000);
+numList.push(-1);
+console.log(numList);
