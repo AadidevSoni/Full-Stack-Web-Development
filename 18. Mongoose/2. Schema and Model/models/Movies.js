7e01ac8754e57a824aba066c5b5736d724ce0de6
Aadidev Soni <aadidevbahrain@gmail.com>
2025-06-23 17:47:59 +0300

    Mongoose Schema and Model

---
+++ 18. Mongoose/2. Schema and Model/models/Movies.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+//Define Schma (blueprint for collection)
+const movieSchema = new mongoose.Schema({
+  name: {type:String, required:true, trim:true},
+  ratings: {type:Number, required:true, min:1, max:5},
+  money: {
+    type:mongoose.Decimal128,
+    required: true,
+    validate: v => v >= 10,
+  },
+  genre: {type:Array},
+  isActive: {type:Boolean},
+  comments: [{value:{type:String}, published: {type:Date, default: Date.now}},],
+});
+
+//Creating Model
+const movieModel = mongoose.model("Movie",movieSchema); //Converts it into movies
+
+export default movieModel;
